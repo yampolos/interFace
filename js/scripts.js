57227424b0ba52ae17d246e92cf5317cb221605b
nabil <yampolos@gmail.com>
2016-12-17 21:11:12 +0100

    model page unique creation modification consultation

--- js/scripts.js
+++ js/scripts.js
@@ -5,43 +5,43 @@ $(document).ready(function() {
 	    myScroll = new IScroll('#wrapper');
 	};
 
-
-	$.fn.editable.defaults.ajaxOptions = {type: "PUT"};
-
-	$.fn.editable.defaults.mode = 'inline';
-
     $('.drawer').drawer();
-    $('#username').editable();
-    $('#ddn').editable();
 
-    $.fn.datepicker.dates['fr'] = {
-    days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
-    daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
-    daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa" ],
-    months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"],
-    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    today: "Today",
-    clear: "Clear",
-    format: "mm/dd/yyyy",
-    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
-    weekStart: 0
-};
-
-
-$('.datepicker').datepicker({
-    language: 'fr'
-});
-
-
-
-    $(function(){
-        $('#status').editable({
-            value: 2,    
-            source: [
-                  {value: 1, text: 'Autres'},
-                  {value: 2, text: 'Femme'},
-                  {value: 3, text: 'Homme'}
-               ]
-        });
+    $('#action').click(function() {
+        $.fn.editable.defaults.mode = 'inline';
+        $(this).editable({
+                value: 2,    
+                source: [
+                      {value: 1, text: 'Flux'},
+                      {value: 2, text: 'Numérique'},
+                      {value: 3, text: 'Annonceur Sentinnelle'}
+                   ]
+            });
     });
+
+    $('.edit').click(function(event) {
+        $.fn.editable.defaults.mode = 'inline';
+        $(this).editable();
+
+
+
+ 
+        event.stopPropagation();
+    });
+
+/* le drawer    $(".btn-tiroire").click(function() {
+        $(this).animate({
+            left: '90%',});
+
+    });
+    
+    $(".fa-close").click(function(event) {
+        var par = $(event.target).parent();
+        $(par).animate({
+            left: '0',
+        });
+        event.stopPropagation();
+    });  */
+
+
 });
